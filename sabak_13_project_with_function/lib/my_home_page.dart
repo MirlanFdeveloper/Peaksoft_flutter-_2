31534de82280b8e368b345be318f9accd2cabf33
MirlanFdeveloper <miki02101981@gmail.com>
2024-04-11 23:38:55 +0600

    dicrement increment

--- sabak_13_project_with_function/lib/my_home_page.dart
+++ sabak_13_project_with_function/lib/my_home_page.dart
@@ -8,19 +8,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  
   int count = 0;
 
-  void increment(){
-    setState(() {
-      count++;
-    });
+  void increment(int san){
+    san=count++;
+    setState(() {});
     
   }
-  void dicrement(){
-      setState(() {
-        count--;
-      });
+  void dicrement(int san){
+    san=count--;
+      setState(() {});
       
+    }
+    void result(){
+      if(count==10){
+        showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('Сиздин сан'),
+          content: Text(count.toString()),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+        
+      }
     }
   @override
   Widget build(BuildContext context) {
@@ -33,19 +54,46 @@ class _MyHomePageState extends State<MyHomePage> {
               count.toString(),
               style: const TextStyle(fontSize: 30),
             ),
-            ElevatedButton(
-              onPressed: (){
-                increment();
-              },
-             child: const Text('Increment'),
-             ),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                ElevatedButton(
+                
+                  
+                onPressed: (){
+                  increment(count);
+                  result();
+                },
+               child:  const Text('Increment'),
+               style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey,
+                foregroundColor: Colors.black,
 
-             ElevatedButton(
-              onPressed: (){
-                dicrement();
-              },
-              child: const Text('Dicrement'),
-             ),
+               ) ,
+              
+               ),
+               
+
+              const SizedBox(width: 20,),
+
+               ElevatedButton(
+                onPressed: (){
+                  dicrement(count);
+                  result();
+                },
+                child: const Text('Dicrement'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey,
+                  foregroundColor: Colors.black,
+                  elevation: 10.0,
+                  shadowColor: Colors.grey.withOpacity(0.5),
+                ),
+               ),
+              ],
+              ),
+            ),
+         
           ],
         ),
       ),
